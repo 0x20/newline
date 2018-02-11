1623d3d10d6014023b92e53a31a5483e6e3608e2
Dieter Adriaenssens <ruleant@users.sourceforge.net>
2018-02-11 15:16:54 +0100

    reorganize code into functions

--- schedule_parser.py
+++ schedule_parser.py
@@ -37,64 +37,80 @@ from pentabarf.Day import Day
 from pentabarf.Room import Room
 from pentabarf.Event import Event
 
-CURRENT_YEAR = datetime.now().strftime("%Y")
+def main():
+    """ Main function, reads the json file and generates the Pentabarf XML """
 
-jsonInputFile = '%s/json/data.json' % (CURRENT_YEAR)
-pentabarfFile = '%s/xml/pentabarf.xml' % (CURRENT_YEAR)
+    current_year = datetime.now().strftime("%Y")
 
-conferenceName = "Newline 0x08"
-conferenceVenue = "Hackerspace.Gent"
-conferenceCity = "Ghent"
-conferenceStart = "2018-04-13"
-conferenceEnd = "2018-04-15"
-conferenceDayChange = "06:00"
-conferenceTimeslotDuration = "00:30"
+    json_input_file = '%s/json/data.json' % (current_year)
+    pentabarf_file = '%s/xml/pentabarf.xml' % (current_year)
 
-startDay = datetime.strptime(conferenceStart, "%Y-%m-%d")
-endDay = datetime.strptime(conferenceEnd, "%Y-%m-%d")
-conferenceDays = int((abs(endDay - startDay)).total_seconds() / (3600 * 24)) + 1
+    conference = read_json(json_input_file)
+    generate_pentabarf_xml(conference, pentabarf_file)
 
-conf = Conference(
-    title=conferenceName,
-    venue=conferenceVenue,
-    city=conferenceCity,
-    start=startDay,
-    end=endDay,
-    days=conferenceDays,
-    day_change=conferenceDayChange,
-    timeslot_duration=conferenceTimeslotDuration
-)
+def read_json(json_input_file):
+    """ Read and parse the conference schedule JSON file """
 
-for i in range(0, conferenceDays):
-    tempDate = startDay + timedelta(days=i)
-    tempDateString = tempDate.strftime("%Y-%m-%d")
-    tempDay = Day(tempDate, tempDateString)
-    tempDay.add_room(Room(conferenceVenue))
-    conf.add_day(tempDay)
+    conferenceName = "Newline 0x08"
+    conferenceVenue = "Hackerspace.Gent"
+    conferenceCity = "Ghent"
+    conferenceStart = "2018-04-13"
+    conferenceEnd = "2018-04-15"
+    conferenceDayChange = "06:00"
+    conferenceTimeslotDuration = "00:30"
 
-if not isfile(jsonInputFile):
-    print "Inputfile '%s' does not exist" % jsonInputFile
-    exit()
+    startDay = datetime.strptime(conferenceStart, "%Y-%m-%d")
+    endDay = datetime.strptime(conferenceEnd, "%Y-%m-%d")
+    conferenceDays = int((abs(endDay - startDay)).total_seconds() / (3600 * 24)) + 1
 
-with open(jsonInputFile) as json_data:
-    newline_data = json.load(json_data)
-
-    for event in newline_data:
-        tmpEventDate = datetime.fromtimestamp(event['start'])
-        tmpEvent = Event(
-            title=event['name'],
-            description=event['description'],
-            type=event['type'],
-            duration=time.strftime("%H:%M:%S", time.gmtime(event['duration'])),
-            date=tmpEventDate,
-            start=tmpEventDate.strftime("%H:%M:%S")
+    conf = Conference(
+        title=conferenceName,
+        venue=conferenceVenue,
+        city=conferenceCity,
+        start=startDay,
+        end=endDay,
+        days=conferenceDays,
+        day_change=conferenceDayChange,
+        timeslot_duration=conferenceTimeslotDuration
         )
 
-        for day in conf.day_objects:
-            if tmpEventDate.strftime("%Y-%m-%d") == day.date.strftime("%Y-%m-%d"):
-                day.room_objects[0].add_event(tmpEvent)
+    for i in range(0, conferenceDays):
+        tempDate = startDay + timedelta(days=i)
+        tempDateString = tempDate.strftime("%Y-%m-%d")
+        tempDay = Day(tempDate, tempDateString)
+        tempDay.add_room(Room(conferenceVenue))
+        conf.add_day(tempDay)
 
-    schedule = conf.generate()
+    if not isfile(json_input_file):
+        print "Inputfile '%s' does not exist" % json_input_file
+        exit()
 
-    with open(pentabarfFile, 'w') as xml_export:
+    with open(json_input_file) as json_data:
+        newline_data = json.load(json_data)
+
+        for event in newline_data:
+            tmpEventDate = datetime.fromtimestamp(event['start'])
+            tmpEvent = Event(
+                title=event['name'],
+                description=event['description'],
+                type=event['type'],
+                duration=time.strftime("%H:%M:%S", time.gmtime(event['duration'])),
+                date=tmpEventDate,
+                start=tmpEventDate.strftime("%H:%M:%S")
+                )
+
+            for day in conf.day_objects:
+                if tmpEventDate.strftime("%Y-%m-%d") == day.date.strftime("%Y-%m-%d"):
+                    day.room_objects[0].add_event(tmpEvent)
+
+        return conf
+
+def generate_pentabarf_xml(conf_schedule, xml_file):
+    """ Generate and write Pentabarf XML file """
+    schedule = conf_schedule.generate()
+
+    with open(xml_file, 'w') as xml_export:
         xml_export.write(schedule)
+
+if __name__ == "__main__":
+    main()
